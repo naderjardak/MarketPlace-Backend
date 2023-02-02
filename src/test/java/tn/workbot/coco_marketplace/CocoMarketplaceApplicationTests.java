01faece2682947d6bc91e28317be35fdf493d5c7
Exon0 <ilyes.bettaieb@esprit.tn>
2023-02-02 17:33:10 +0100

    Project initialized

---
+++ src/test/java/tn/workbot/coco_marketplace/CocoMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.workbot.coco_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
