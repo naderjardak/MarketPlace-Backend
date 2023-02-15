e92782d79590c0fb81833200b9ea9e0486e9a9f3
mohsenfennira <mohsen.fennira@esprit.tn>
2023-02-16 00:46:08 +0100

    test

--- src/main/java/entities/AgencyBranch.java
+++ src/main/java/entities/AgencyBranch.java
@@ -1,4 +1,14 @@
 package entities;
 
-public class AgencyBranch {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class AgencyBranch implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
