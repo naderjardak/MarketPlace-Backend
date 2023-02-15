55dde1c0a83a9ed791a535687924385c6b805e3c
Exon0 <ilyes.bettaieb@esprit.tn>
2023-02-16 00:40:02 +0100

    test

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,4 +1,13 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+
 }
