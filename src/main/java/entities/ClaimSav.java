140b18da78a48b91d5b6da7c37b5e888363b0842
houssembribech <houssem.bribech@esprit.tn>
2023-02-17 00:38:20 +0100

    Addedattributes

--- src/main/java/entities/ClaimSav.java
+++ src/main/java/entities/ClaimSav.java
@@ -1,15 +1,28 @@
 package entities;
 
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class ClaimSav implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    Long id;
+    private Long id;
+
+    private String reference;
+
+    private String description;
+
+    private String status;
+
 }
