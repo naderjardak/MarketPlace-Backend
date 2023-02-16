18ab30a1b7242e76ad019251f9848b20077dbf43
houssembribech <houssem.bribech@esprit.tn>
2023-02-16 23:21:33 +0100

    OpenBranch

--- src/main/java/entities/ClaimSav.java
+++ src/main/java/entities/ClaimSav.java
@@ -1,4 +1,15 @@
 package entities;
 
-public class ClaimSav {
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class ClaimSav implements Serializable {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    Long id;
 }
