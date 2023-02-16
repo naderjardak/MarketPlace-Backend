a88d132519c515bc79d18e8ba392b87e59098dce
naderjardak <naderjardak5@gmail.com>
2023-02-17 00:33:42 +0100

    add order payementtype productQuentity shipping statusOrder

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,13 +1,31 @@
 package entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-public class Order {
+public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idOrder;
+    private Long id;
+
+    private String ref;
+    private float sum;
+    private String orderCode;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentType payment;
+
+    @Enumerated(EnumType.STRING)
+    private StatusOrderType status;
+
+    @Temporal(TemporalType.TIME)
+    private Date creationDate;
 }
---
+++ src/main/java/entities/ProductQuantity.java
@@ -0,0 +1,26 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ProductQuantity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+}
