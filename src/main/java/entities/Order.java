41f5088c6c0a03a89f30f73ca4a089901ce8e248
naderjardak <naderjardak5@gmail.com>
2023-02-16 23:05:06 +0100

    open branch

--- src/main/java/entities/Order.java
+++ src/main/java/entities/Order.java
@@ -1,4 +1,13 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idOrder;
 }
