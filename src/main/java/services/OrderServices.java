09e359a7582bf7c8586ec1ca678fa2dbf2807898
naderjardak <naderjardak5@gmail.com>
2023-02-17 23:53:55 +0100

    AutoWired added

--- src/main/java/services/OrderServices.java
+++ src/main/java/services/OrderServices.java
@@ -1,9 +1,13 @@
 package services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repositories.OrderRepository;
 
 @Service
 @Slf4j
 public class OrderServices implements OrderInterface {
+    @Autowired
+    OrderRepository orderRepository;
 }
--- src/main/java/services/ShippingServices.java
+++ src/main/java/services/ShippingServices.java
@@ -1,9 +1,14 @@
 package services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repositories.ShippingRepository;
 
 @Service
 @Slf4j
 public class ShippingServices implements ShippingInterface{
+    @Autowired
+    ShippingRepository shippingRepository;
+
 }
