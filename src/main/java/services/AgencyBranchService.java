a4acacb7e8e9c88d9e8a8799d53441d5af41f03a
mohsenfennira <mohsen.fennira@esprit.tn>
2023-02-17 23:53:43 +0100

    Entities(Pickup+AgencyBranch+AgencyDeliveryMan) + Attributs + Repository +Services +IServices +Controllers

--- src/main/java/services/AgencyBranchService.java
+++ src/main/java/services/AgencyBranchService.java
@@ -1,7 +1,11 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgencyBranchService {
+    @Autowired
+    AgencyBranchIService abi;
+
 }
--- src/main/java/services/AgencyDeliveryManService.java
+++ src/main/java/services/AgencyDeliveryManService.java
@@ -1,7 +1,10 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgencyDeliveryManService {
+    @Autowired
+    AgencyDeliveryManIService admi;
 }
