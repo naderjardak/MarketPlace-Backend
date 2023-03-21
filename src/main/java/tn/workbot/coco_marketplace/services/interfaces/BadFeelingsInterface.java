3380132eee212942b033014e9bfd817ee2b0946a
houssembribech <houssem.bribech@esprit.tn>
2023-03-21 23:04:12 +0100

    Lastcommit5

--- src/main/java/tn/workbot/coco_marketplace/services/interfaces/BadFeelingsInterface.java
+++ src/main/java/tn/workbot/coco_marketplace/services/interfaces/BadFeelingsInterface.java
@@ -2,7 +2,15 @@ package tn.workbot.coco_marketplace.services.interfaces;
 
 import tn.workbot.coco_marketplace.entities.BadFeelings;
 
+import java.util.List;
+
 public interface BadFeelingsInterface {
 
     public void addNewBadFeelings(BadFeelings badFeelings);
+
+    public void updateBadFeelings(BadFeelings badFeelings);
+
+    public void deleteBadFeelings(Long id);
+
+    public List<BadFeelings> getAllBadFeelings();
 }
